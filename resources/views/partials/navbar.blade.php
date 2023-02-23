828eecf629a48536b495d8b9e0ddeb858fdaabe0
Deceulaer Adrien <76573341+DeceulaerAdrien@users.noreply.github.com>
2023-02-23 20:31:17 +0100

    css de *

--- resources/views/partials/navbar.blade.php
+++ resources/views/partials/navbar.blade.php
@@ -10,17 +10,13 @@
         <div class="menu">
             <li><a href="/">Home</a></li>
             <li><a href="/">About</a></li>
-            <li class="services"><a href="/"> Services </a>
+            <li class="service"><a href="/">Services</a>
                 <ul class="dropdown">
                     <li><a href="/">Dropdown 1 </a></li>
-
                     <li><a href="/">Dropdown 2</a></li>
-
-                    <li><a href="/">Dropdown 2</a></li>
-
                     <li><a href="/">Dropdown 3</a></li>
-
                     <li><a href="/">Dropdown 4</a></li>
+                    <li><a href="/">Dropdown 5</a></li>
                 </ul>
             </li>
             <li><a href="/">Pricing</a></li>
