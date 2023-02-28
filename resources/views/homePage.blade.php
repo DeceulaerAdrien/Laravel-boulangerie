3c9c028b7ba4bf7be397d9621dc13d3b7c7fdde6
DeceulaerAdrien <adriendeceulaer@gmail.com>
2023-02-28 12:05:50 +0100

    carousel dure

--- resources/views/homePage.blade.php
+++ resources/views/homePage.blade.php
@@ -5,47 +5,67 @@
 
 
 @section('Content')
-    <div class="container">
-        {{-- carousel d'image --}}
-        <div id="carousel" class="carousel slide" data-bs-ride="carousel">
-            <div class="carousel-indicators">
-                <button type="button" data-bs-target="#carousel" data-bs-slide-to="0" class="active" aria-current="true"
-                    aria-label="Slide 1"></button>
-                <button type="button" data-bs-target="#carousel" data-bs-slide-to="1" aria-label="Slide 2"></button>
-                <button type="button" data-bs-target="#carousel" data-bs-slide-to="2" aria-label="Slide 3"></button>
-            </div>
-            <div class="carousel-inner">
-                <div class="carousel-item active">
-                    <img src="{{ asset('image/exemple-carousel-1.jpg') }}" class="d-block w-100" alt="exemple_1">
-                    <div class="carousel-caption">
-                        <h5>First slide label</h5>
-                        <p>Some representative placeholder content for the slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('image/exemple-carousel-2.jpg') }}" class="d-block w-100" alt="exemple_2">
-                    <div class="carousel-caption">
-                        <h5>Second slide label</h5>
-                        <p>Some representative placeholder content for the slide.</p>
-                    </div>
-                </div>
-                <div class="carousel-item">
-                    <img src="{{ asset('image/exemple-carousel-3.jpg') }}" class="d-block w-100" alt="exemple_3">
-                    <div class="carousel-caption">
-                        <h5>Third slide label</h5>
-                        <p>Some representative placeholder content for the slide.</p>
-                    </div>
-                </div>
-            </div>
-            <button class="carousel-control-prev" type="button" data-bs-target="#carousel" data-bs-slide="prev">
-                <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Previous</span>
-            </button>
-            <button class="carousel-control-next" type="button" data-bs-target="#carousel" data-bs-slide="next">
-                <span class="carousel-control-next-icon" aria-hidden="true"></span>
-                <span class="visually-hidden">Next</span>
-            </button>
+    {{-- carousel d'image --}}
+    <div class="slideshow">
+
+        <div class="mySlides fade">
+            <div class="numbertext">1 / 3</div>
+            <img src="{{ asset('image/img-1.jpg') }}" alt="img-1">
+            <div class="text">Caption Text</div>
         </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">2 / 3</div>
+            <img src="{{ asset('image/img-2.jpg') }}" alt="img-2">
+            <div class="text">Caption Two</div>
+        </div>
+
+        <div class="mySlides fade">
+            <div class="numbertext">3 / 3</div>
+            <img src="{{ asset('image/img-3.jpg') }}" alt="img-3">
+            <div class="text">Caption Three</div>
+        </div>
+        <a class="next" onclick="nextSlide(1)">&#10095;</a>
+        <a class="prev" onclick="nextSlide(-1)">&#10094;</a>
+    </div>
+    <div style="text-align:center">
+        <span class="dot" onclick="currentSlide(1)"></span>
+        <span class="dot" onclick="currentSlide(2)"></span>
+        <span class="dot" onclick="currentSlide(3)"></span>
     </div>
 
+    <script>
+        let slideIndex = 1;
+        showSlides(slideIndex);
+
+        // Next/previous controls
+        function nextSlide(n) {
+            showSlides((slideIndex += n));
+        }
+
+        // Thumbnail image controls
+        function currentSlide(n) {
+            showSlides((slideIndex = n));
+        }
+
+        function showSlides(n) {
+            let i;
+            let slides = document.getElementsByClassName("mySlides");
+            let dots = document.getElementsByClassName("dot");
+            if (n > slides.length) {
+                slideIndex = 1;
+            }
+            if (n < 1) {
+                slideIndex = slides.length;
+            }
+            for (i = 0; i < slides.length; i++) {
+                slides[i].style.display = "none";
+            }
+            for (i = 0; i < dots.length; i++) {
+                dots[i].className = dots[i].className.replace(" active", "");
+            }
+            slides[slideIndex - 1].style.display = "block";
+            dots[slideIndex - 1].className += " active";
+        }
+    </script>
 @endsection
