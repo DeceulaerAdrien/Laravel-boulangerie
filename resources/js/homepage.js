e4c5e6a1270e5ca87db9e7a9535cf6dfce464962
DeceulaerAdrien <adriendeceulaer@gmail.com>
2023-05-07 17:17:03 +0200

    optimisation js

--- resources/js/homepage.js
+++ resources/js/homepage.js
@@ -1,34 +1,30 @@
 let slideIndex = 1;
-let nextButton = document.getElementById("next");
-let previousButton = document.getElementById("previous");
-let numberText = document.getElementsByClassName("numbertext");
-let slides = document.getElementsByClassName("mySlides");
-let dots = document.getElementById("dots");
-let dot = document.getElementsByClassName("dot");
+const nextButton = document.querySelector("#next");
+const previousButton = document.querySelector("#previous");
+const numberText = document.querySelectorAll(".numbertext");
+const slides = document.querySelectorAll(".mySlides");
+const dots = document.querySelector("#dots");
+const dot = document.querySelectorAll(".dot");
 
 // Next/previous controls
 function nextSlide(n) {
-    showSlides((slideIndex += n));
+    showSlides(slideIndex + n);
 }
 
 // Thumbnail image controls
 function showSlides(n) {
-    if (n > slides.length) {
-        slideIndex = 1;
-    }
-    if (n < 1) {
-        slideIndex = slides.length;
-    }
-    for (let i = 0; i < slides.length; i++) {
-        slides[i].style.display = "none";
-    }
+    slideIndex = n > slides.length ? 1 : n < 1 ? slides.length : n;
 
-    for (let i = 0; i < dot.length; i++) {
-        dot[i].className = dot[i].className.replace("active", "");
-    }
+    slides.forEach((slide) => {
+        slide.style.display = "none";
+    });
+
+    dot.forEach((dotItem) => {
+        dotItem.classList.remove("active");
+    });
 
     slides[slideIndex - 1].style.display = "block";
-    dot[slideIndex - 1].className += " active";
+    dot[slideIndex - 1].classList.add("active");
     numberText[slideIndex - 1].innerHTML = `${slideIndex}/${slides.length}`;
 }
 
