d3df7001460557529fdf34c3f9f79a6167421790
Deceulaer Adrien <76573341+DeceulaerAdrien@users.noreply.github.com>
2023-08-25 13:51:06 +0200

    home page js

--- resources/views/partials/slideshow.blade.php
+++ resources/views/partials/slideshow.blade.php
@@ -2,12 +2,17 @@
     <div class="slideshow">
 
         <div id="mySlides" class="mySlides fade">
+            <img src="" alt="test 1">
+            <p class="numbertext"></p>
         </div>
 
 
         <div id="mySlides" class="mySlides fade">
+            <img src="" alt="test 2">
+            <p class="numbertext"></p>
         </div>
 
         <a class="next" id="next">&#10095;</a>
         <a class="prev" id="previous">&#10094;</a>
+
     </div>
